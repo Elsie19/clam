48514813039f1998f11c9fa819284be309eea3b4
Henry Wenger-Stickel <hwengerstickel@protonmail.com>
2023-02-13 16:54:04 -0600

    Do not split prompt.{input,yes_no} input

--- headers/prompt.sh
+++ headers/prompt.sh
@@ -4,7 +4,7 @@ function prompt.input() {
     local prompt="${1:?variable for prompt for prompt_input not given}"
     local var_to_store_to="${2:?variable to store output for prompt_input not given}"
     echo -ne "${1}: "
-    read -r prompt_output
+    IFS= read -r prompt_output
     declare -g "${var_to_store_to}=${prompt_output}"
     unset prompt_output
     return 0
@@ -14,7 +14,7 @@ function prompt.yes_no() {
     local prompt="${1:?variable for prompt for prompt_yes_no not given}"
     local var_to_store_to="${2:?variable to store output for prompt_yes_no not given}"
     echo -ne "${prompt} [${BGreen}y${NC}/${BRed}n${NC}]: "
-    read -r prompt_output
+    IFS= read -r prompt_output
     case "${prompt_output}" in
         [Yy]*)
             declare -g "${var_to_store_to}=Y"
