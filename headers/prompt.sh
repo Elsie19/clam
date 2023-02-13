211062e921328a04b60efe6e550b69f6258afb53
Henry Wenger-Stickel <hwengerstickel@protonmail.com>
2023-02-12 20:26:43 -0600

    Add variable to `prompt_input`

--- headers/prompt.sh
+++ headers/prompt.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 function prompt_input() {
-    echo -ne "${1:?}: "
+    local prompt="${1:?variable for prompt for prompt_input not given}"
+    local var_to_store_to="${2:?variable to store output for prompt_input not given}"
+    echo -ne "${1}: "
     read -r prompt_output
-    echo "${prompt_output}"
+    declare -g "${var_to_store_to}=${prompt_output}"
+    unset prompt_output
 }
 
 function prompt_yes_no() {
@@ -13,10 +16,14 @@ function prompt_yes_no() {
     read -r prompt_output
     case "${prompt_output}" in
         [Yy]*)
-	    declare -g "${var_to_store_to}=Y"
+            declare -g "${var_to_store_to}=Y"
+            unset prompt_output
+            return 0
             ;;
         [Nn]*)
-	    declare -g "${var_to_store_to}=N"
+            declare -g "${var_to_store_to}=N"
+            unset prompt_output
+            return 0
             ;;
         *)
             return 1
