49161fcb9f6ed89237c25df87ea6246ad61e639a
Henry Wenger-Stickel <hwengerstickel@protonmail.com>
2023-02-14 19:09:07 -0600

    Readonly colors

--- headers/colors.sh
+++ headers/colors.sh
@@ -2,86 +2,88 @@
 
 # Colors
 BOLD=$(tput bold)
-export BOLD
+declare -r BOLD
 NORMAL=$(tput sgr0)
-export NORMAL
-export NC='\033[0m'
+declare -r NORMAL
+declare -r NC='\033[0m'
+
+export BOLD NORMAL NC
 # Courtesy of https://stackoverflow.com/a/28938235/13449010
 
 # https://no-color.org/
 # shellcheck disable=SC2236
-if [[ ! -n ${NO_COLOR} ]]; then
+if [[ -z ${NO_COLOR} ]]; then
     # Regular Colors
-    export BLACK='\033[0;30m'  # Black
-    export RED='\033[0;31m'    # Red
-    export GREEN='\033[0;32m'  # Green
-    export YELLOW='\033[0;33m' # Yellow
-    export BLUE='\033[0;34m'   # Blue
-    export PURPLE='\033[0;35m' # Purple
-    export CYAN='\033[0;36m'   # Cyan
-    export WHITE='\033[0;37m'  # White
+    declare -r BLACK='\033[0;30m'  # Black
+    declare -r RED='\033[0;31m'    # Red
+    declare -r GREEN='\033[0;32m'  # Green
+    declare -r YELLOW='\033[0;33m' # Yellow
+    declare -r BLUE='\033[0;34m'   # Blue
+    declare -r PURPLE='\033[0;35m' # Purple
+    declare -r CYAN='\033[0;36m'   # Cyan
+    declare -r WHITE='\033[0;37m'  # White
 
     # Bold
-    export BBlack='\033[1;30m'  # Black
-    export BRed='\033[1;31m'    # Red
-    export BGreen='\033[1;32m'  # Green
-    export BYellow='\033[1;33m' # Yellow
-    export BBlue='\033[1;34m'   # Blue
-    export BPurple='\033[1;35m' # Purple
-    export BCyan='\033[1;36m'   # Cyan
-    export BWhite='\033[1;37m'  # White
+    declare -r BBlack='\033[1;30m'  # Black
+    declare -r BRed='\033[1;31m'    # Red
+    declare -r BGreen='\033[1;32m'  # Green
+    declare -r BYellow='\033[1;33m' # Yellow
+    declare -r BBlue='\033[1;34m'   # Blue
+    declare -r BPurple='\033[1;35m' # Purple
+    declare -r BCyan='\033[1;36m'   # Cyan
+    declare -r BWhite='\033[1;37m'  # White
 
     # Underline
-    export UBlack='\033[4;30m'  # Black
-    export URed='\033[4;31m'    # Red
-    export UGreen='\033[4;32m'  # Green
-    export UYellow='\033[4;33m' # Yellow
-    export UBlue='\033[4;34m'   # Blue
-    export UPurple='\033[4;35m' # Purple
-    export UCyan='\033[4;36m'   # Cyan
-    export UWhite='\033[4;37m'  # White
+    declare -r UBlack='\033[4;30m'  # Black
+    declare -r URed='\033[4;31m'    # Red
+    declare -r UGreen='\033[4;32m'  # Green
+    declare -r UYellow='\033[4;33m' # Yellow
+    declare -r UBlue='\033[4;34m'   # Blue
+    declare -r UPurple='\033[4;35m' # Purple
+    declare -r UCyan='\033[4;36m'   # Cyan
+    declare -r UWhite='\033[4;37m'  # White
 
     # Background
-    export On_Black='\033[40m'  # Black
-    export On_Red='\033[41m'    # Red
-    export On_Green='\033[42m'  # Green
-    export On_Yellow='\033[43m' # Yellow
-    export On_Blue='\033[44m'   # Blue
-    export On_Purple='\033[45m' # Purple
-    export On_Cyan='\033[46m'   # Cyan
-    export On_White='\033[47m'  # White
+    declare -r On_Black='\033[40m'  # Black
+    declare -r On_Red='\033[41m'    # Red
+    declare -r On_Green='\033[42m'  # Green
+    declare -r On_Yellow='\033[43m' # Yellow
+    declare -r On_Blue='\033[44m'   # Blue
+    declare -r On_Purple='\033[45m' # Purple
+    declare -r On_Cyan='\033[46m'   # Cyan
+    declare -r On_White='\033[47m'  # White
 
     # High Intensity
-    export IBlack='\033[0;90m'  # Black
-    export IRed='\033[0;91m'    # Red
-    export IGreen='\033[0;92m'  # Green
-    export IYellow='\033[0;93m' # Yellow
-    export IBlue='\033[0;94m'   # Blue
-    export IPurple='\033[0;95m' # Purple
-    export ICyan='\033[0;96m'   # Cyan
-    export IWhite='\033[0;97m'  # White
+    declare -r IBlack='\033[0;90m'  # Black
+    declare -r IRed='\033[0;91m'    # Red
+    declare -r IGreen='\033[0;92m'  # Green
+    declare -r IYellow='\033[0;93m' # Yellow
+    declare -r IBlue='\033[0;94m'   # Blue
+    declare -r IPurple='\033[0;95m' # Purple
+    declare -r ICyan='\033[0;96m'   # Cyan
+    declare -r IWhite='\033[0;97m'  # White
 
     # Bold High Intensity
-    export BIBlack='\033[1;90m'  # Black
-    export BIRed='\033[1;91m'    # Red
-    export BIGreen='\033[1;92m'  # Green
-    export BIYellow='\033[1;93m' # Yellow
-    export BIBlue='\033[1;94m'   # Blue
-    export BIPurple='\033[1;95m' # Purple
-    export BICyan='\033[1;96m'   # Cyan
-    export BIWhite='\033[1;97m'  # White
+    declare -r BIBlack='\033[1;90m'  # Black
+    declare -r BIRed='\033[1;91m'    # Red
+    declare -r BIGreen='\033[1;92m'  # Green
+    declare -r BIYellow='\033[1;93m' # Yellow
+    declare -r BIBlue='\033[1;94m'   # Blue
+    declare -r BIPurple='\033[1;95m' # Purple
+    declare -r BICyan='\033[1;96m'   # Cyan
+    declare -r BIWhite='\033[1;97m'  # White
 
     # High Intensity backgrounds
-    export On_IBlack='\033[0;100m'  # Black
-    export On_IRed='\033[0;101m'    # Red
-    export On_IGreen='\033[0;102m'  # Green
-    export On_IYellow='\033[0;103m' # Yellow
-    export On_IBlue='\033[0;104m'   # Blue
-    export On_IPurple='\033[0;105m' # Purple
-    export On_ICyan='\033[0;106m'   # Cyan
-    export On_IWhite='\033[0;107m'  # White
+    declare -r On_IBlack='\033[0;100m'  # Black
+    declare -r On_IRed='\033[0;101m'    # Red
+    declare -r On_IGreen='\033[0;102m'  # Green
+    declare -r On_IYellow='\033[0;103m' # Yellow
+    declare -r On_IBlue='\033[0;104m'   # Blue
+    declare -r On_IPurple='\033[0;105m' # Purple
+    declare -r On_ICyan='\033[0;106m'   # Cyan
+    declare -r On_IWhite='\033[0;107m'  # White
 else
-    export BLACK RED GREEN YELLOW BLUE PURPLE CYAN WHITE \
+    declare -r BLACK RED GREEN YELLOW BLUE PURPLE CYAN WHITE \
         BBlack BRed BGreen BYellow BBlue BPurple BCyan BWhite \
         UBlack URed UGreen UYellow UBlue UPurple UCyan UWhite \
         On_Black On_Red On_Green On_Yellow On_Blue On_Purple On_Cyan On_White \
@@ -89,3 +91,11 @@ else
         BIBlack BIRed BIGreen BIYellow BIBlue BIPurple BICyan BIWhite \
         On_IBlack On_IRed On_IGreen On_IYellow On_IBlue On_IPurple On_ICyan On_IWhite
 fi
+
+export BLACK RED GREEN YELLOW BLUE PURPLE CYAN WHITE \
+    BBlack BRed BGreen BYellow BBlue BPurple BCyan BWhite \
+    UBlack URed UGreen UYellow UBlue UPurple UCyan UWhite \
+    On_Black On_Red On_Green On_Yellow On_Blue On_Purple On_Cyan On_White \
+    IBlack IRed IGreen IYellow IBlue IPurple ICyan IWhite \
+    BIBlack BIRed BIGreen BIYellow BIBlue BIPurple BICyan BIWhite \
+    On_IBlack On_IRed On_IGreen On_IYellow On_IBlue On_IPurple On_ICyan On_IWhite
