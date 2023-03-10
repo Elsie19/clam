85265bc7dbed5beac9efbd48769c2a388a343419
Henry Wenger-Stickel <hwengerstickel@protonmail.com>
2023-03-09 20:45:53 -0600

    Add `use.sh` guards

--- headers/use.sh
+++ headers/use.sh
@@ -11,6 +11,16 @@ function use() {
     if [[ ${full_name} == "${to_be}" ]]; then
         return 0
     fi
+    if [[ "$(type -t "${to_be}")" == "alias" ]]; then
+        echo "'${to_be}' is already a defined alias" >&2
+        exit 1
+    elif [[ "$(type -t "${to_be}")" == "function" ]]; then
+        shopt -s extdebug
+        mapfile -d " " whereis < <(declare -Ff "${to_be}")
+        echo "'${to_be}' is already a defined function on line ${whereis[1]}" >&2
+        shopt -u extdebug
+        exit 1
+    fi
     # shellcheck disable=SC2139
     alias "${to_be}"="${full_name}"
 }
