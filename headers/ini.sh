4f9fa84c65fab9e5302b76ab86f38067bd968ed1
Henry Wenger-Stickel <hwengerstickel@protonmail.com>
2023-03-11 15:15:40 -0600

    Allow all kinds of whitespace

--- headers/ini.sh
+++ headers/ini.sh
@@ -21,8 +21,8 @@ function ini._parse_out_section() {
 
 function ini._convert_var() {
     local input="${1:?No input given to ini._convert_var}"
-    local var="${input%% = *}"
-    local value="${input#* = }"
+    local var="${input%%=*}"
+    local value="${input#*=}"
     var="${var#"${var%%[![:space:]]*}"}"
     var="${var%"${var##*[![:space:]]}"}"
     value="${value#"${value%%[![:space:]]*}"}"
@@ -33,27 +33,32 @@ function ini._convert_var() {
 function ini.parse() {
     local file="${1:?No input given to ini.parse}"
     # shellcheck disable=SC2034
-    local var_prefix="${2-}"
+    local default_section="${2:-Default}"
 
-    local default_section="Default"
     if ! [[ -f ${file} ]]; then
         return 1
     fi
 
     local ini_var_regex='^(\w)+\s*=\s*(\")?(\w)+(\")?$'
     local ini_section_regex='\[([a-zA-Z0-9_ ])+\]$'
+
+    local line_counter=0
     while IFS= read -r line; do
+        ((line_counter++))
         # Is blank?
         if [[ -z ${line} ]]; then
             continue
         elif [[ ${line} =~ ${ini_section_regex} ]]; then
             if ! ini._parse_out_section "${line}" &> /dev/null; then
-                echo "Invalid line: ${line}!" 2>&1
+                echo "[${line_counter}] Invalid line: ${line}" 2>&1
                 return 1
             fi
             default_section="$(ini._parse_out_section "${line}")"
         elif [[ ${line} =~ ${ini_var_regex} ]]; then
             ini._convert_var "${line}"
+        else
+            echo "[${line_counter}] Invalid line: ${line}" 2>&1
+            return 1
         fi
     done < "${file}"
 }
