d0d610c3f387c3fb9c250c2c4e6a0d09c928dc19
Henry Wenger-Stickel <hwengerstickel@protonmail.com>
2023-03-11 21:39:26 -0600

    rm(deps): `tr`, `head`

--- headers/log.sh
+++ headers/log.sh
@@ -5,9 +5,14 @@ function log.init() {
         return 1
     fi
     if ! command -v mktemp &> /dev/null; then
-        # shellcheck disable=SC2155
-        local random_string="$(tr -dc A-Za-z0-9 < /dev/urandom | head -c 10)"
-        :>"/tmp/tmp.${random_string}"
+        local chars=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
+        local random_string=
+        # shellcheck disable=SC2034
+        for i in {1..10}; do
+            random_string+="${chars:RANDOM%${#chars}:1}"
+        done
+        # shellcheck disable=SC2188
+        > "/tmp/tmp.${random_string}"
         declare -r LOGFILE="/tmp/tmp.${random_string}"
         export LOGFILE
     else
