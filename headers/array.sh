7a59c11bb314b7afb15f863274250d1e62bd2f55
Henry Wenger-Stickel <hwengerstickel@protonmail.com>
2023-03-25 18:18:31 -0500

    Remove `read` from array.string_to_array

--- headers/array.sh
+++ headers/array.sh
@@ -3,14 +3,11 @@
 function array.string_to_array() {
     local input="${1:?No input given to array.string_to_array}"
     local array_to="${2:?No out array given to array.string_to_array}"
-    local built_array=()
 
-    while IFS= read -r -n1 character; do
-        built_array+=("${character}")
-    done < <(echo -n "${input}")
-    # shellcheck disable=SC2155
-    local formatted_output="$(printf "%q " "${built_array[@]}")"
-    declare -ag "${array_to}=(${formatted_output})"
+    for ((i = 0; i < ${#input}; ++i)); do
+        local rematched+=("${input:i:1}")
+    done
+    declare -ag "${array_to}=(${rematched[*]})"
     return $?
 }
 
