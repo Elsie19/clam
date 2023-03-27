5045b07fb12dc575b0e5c4fb162997251085d7f9
Henry Wenger-Stickel <hwengerstickel@protonmail.com>
2023-03-27 17:08:37 -0500

    Rework

--- headers/cmds/cat.sh
+++ headers/cmds/cat.sh
@@ -1,36 +1,30 @@
 #!/bin/bash
 
 function cmds.cat() {
-    local files i end number nonempty lineno
-    while getopts 'Ebn' OPTION; do
+    local files i z end number lineno=1 tabbing
+    while getopts 'En' OPTION; do
         case "${OPTION}" in
             E) end='$' ;;
-            n)
-                # shellcheck disable=SC2034
-                number=1 ;;
-            b)
-                # shellcheck disable=SC2034
-                nonempty=1 ;;
-            ?) echo "Usage: ${FUNCNAME[0]} [-E] [-n] [-b] files" && return 1 ;;
+            n) number=1 ;;
+            ?) echo "Usage: ${FUNCNAME[0]} [-E] [-n] files" && return 1 ;;
         esac
-        shift $((OPTIND - 1))
-        readarray -t files < "${@}"
-        lineno=1
-        for i in "${files[@]}"; do
-            if [[ -n "${number}" || -n "${nonempty}" ]]; then
-                if [[ -n "${nonempty}" ]]; then
-                    if [[ -z "${i}" ]]; then
-                        echo
-                    else
-                        echo -e "     ${lineno}	 ${i}${end}"
-                    fi
-                else
-                    echo -e "     ${lineno}	 ${i}${end}"
-                fi
-            else
-                echo "${i}${end}"
+    done
+    shift $((OPTIND - 1))
+    readarray -t files < "${@}"
+    for i in "${files[@]}"; do
+        # 10 would be 2 chars, 500 would be 3
+        tabbing="$((5 - "${#lineno}"))"
+        if [[ -n ${number} ]]; then
+            for ((z = 0; z <= tabbing; z++)); do
+                echo -n ' '
+            done
+            if [[ -z ${i} ]]; then
+                echo "${lineno}  ${end-}" && ((lineno++)) && continue
             fi
-            ((lineno++))
-        done
+            echo "${lineno}  ${i}${end-}"
+        else
+            echo "${i}${end-}"
+        fi
+        ((lineno++))
     done
 }
