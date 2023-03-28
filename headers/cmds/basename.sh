470e01d05cd3086cf48e20bae78b71ddd8de2f5e
Zakaria Barkouk <b.zakaria@zohomail.com>
2023-03-28 21:53:39 +0000

    Add simple `basename.sh`

--- headers/cmds/basename.sh
+++ headers/cmds/basename.sh
@@ -1,28 +1,12 @@
 #!/bin/bash
 
 function cmds.basename() {
-    local OPTION MULTIPLE SUFFIX ZERO path name
-    while getopts 's:z' OPTION ; do
-        case "$OPTION" in
-            s ) SUFFIX="$OPTARG" ;;
-            z ) ZERO=true ;;
-            ?) echo "Usage: ${FUNCNAME[0]} [-b] PATHS" && return 1 ;;
-        esac
-        shift $(( OPTIND - 1 ))
-    done
+    # Usage: basename "path" ["suffix"]
+    local tmp
 
+    tmp=${1%"${1##*[!/]}"}
+    tmp=${tmp##*/}
+    tmp=${tmp%"${2/"$tmp"}"}
 
-    for path in "${@}" ; do
-        name="${path##*/}"
-        if [ -n "$SUFFIX" ]; then
-            name="${name/\.${SUFFIX}}"
-        fi
-
-        echo -n "$name "
-
-        if [ -z "$ZERO" ]; then
-            echo ""
-        fi
-    done
-
+    printf '%s\n' "${tmp:-/}"
 }
