f4b20d81613e5629d1b961408daead39cddd740c
Henry Wenger-Stickel <hwengerstickel@protonmail.com>
2023-03-28 18:11:30 -0500

    echo and return oneliner

--- headers/cmds/sleep.sh
+++ headers/cmds/sleep.sh
@@ -4,8 +4,7 @@ function cmds.sleep() {
     local time
     time="${1:?No time given to cmds.sleep}"
     if [[ ${time: -1} =~ (m|h|d|s) ]] && [[ ${time::-1} =~ ^[0-9]+(\.[0-9]+)$ ]]; then
-        echo "${FUNCNAME[0]} cannot have decimals when used with 's', 'm', 'h', or 'd'"
-        return 1
+        echo "${FUNCNAME[0]} cannot have decimals when used with 's', 'm', 'h', or 'd'" && return 1
     fi
     case "${time: -1}" in
         s) time="${time::-1}" ;;
