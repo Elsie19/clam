9e791b72f57b866f7d0032b8507d9a82302e6b50
Zakaria Barkouk <b.zakaria@zohomail.com>
2023-03-28 23:25:06 +0000

    Add Simple `basename.sh` (#2)
    Co-authored-by: Plasma <hwengerstickel@protonmail.com>

---
+++ headers/cmds/basename.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function cmds.basename() {
+    # Usage: basename "path" ["suffix"]
+    local tmp
+
+    tmp="${1%"${1##*[!/]}"}"
+    tmp="${tmp##*/}"
+    tmp="${tmp%"${2/"${tmp}"}"}"
+
+    printf '%s\n' "${tmp:-/}"
+}
