e91dc154abcb15e4d2b661a9ce85d4f2c6393c16
Plasma <hwengerstickel@protonmail.com>
2023-03-28 18:23:28 -0500

    Quote stuff

--- headers/cmds/basename.sh
+++ headers/cmds/basename.sh
@@ -4,9 +4,9 @@ function cmds.basename() {
     # Usage: basename "path" ["suffix"]
     local tmp
 
-    tmp=${1%"${1##*[!/]}"}
-    tmp=${tmp##*/}
-    tmp=${tmp%"${2/"$tmp"}"}
+    tmp="${1%"${1##*[!/]}"}"
+    tmp="${tmp##*/}"
+    tmp="${tmp%"${2/"$tmp"}"}"
 
     printf '%s\n' "${tmp:-/}"
 }
