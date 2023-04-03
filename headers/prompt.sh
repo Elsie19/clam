938153a6336df03d567fabc411d1b74c8782e92b
Henry Wenger-Stickel <hwengerstickel@protonmail.com>
2023-04-03 18:54:55 -0500

    Add `prompt.sh`

--- headers/prompt.sh
+++ headers/prompt.sh
@@ -1,29 +1,53 @@
 #!/bin/bash
+# @file prompt.sh
+# @brief A library for interacting with the user.
 
+# @section Functions
+# @description Functions for interacting with the user.
+
+# @description Prompts the user for input.
+# @set var_to_store_to Any Prompt input.
+# @stdin The users input
+#
+# @example
+#   prompt.input "Do you like cheese" like_cheese
+#
+# @arg $1 string A prompt.
+# @arg $2 string A variable to save the output to.
 function prompt.input() {
-    local prompt="${1:?variable for prompt for prompt_input not given}"
-    local var_to_store_to="${2:?variable to store output for prompt_input not given}"
+    local prompt var_to_store_to prompt_output
+    prompt="${1:?variable for prompt for prompt_input not given}"
+    var_to_store_to="${2:?variable to store output for prompt_input not given}"
     echo -ne "${1}: "
     IFS= read -r prompt_output
     declare -g "${var_to_store_to}=${prompt_output}"
-    unset prompt_output
     return 0
 }
 
+# @description Prompts the user for a yes or no question.
+# @set var_to_store_to Any Prompt input.
+# @stdin The users input
+#
+# @example
+#   prompt.yes_no "Do you like cheese" yes_no
+#
+# @arg $1 string A prompt.
+# @arg $2 string A variable to save the output to.
+#
+# @exitcode 1 If user input is not `Y` or `N`.
 function prompt.yes_no() {
-    local prompt="${1:?variable for prompt for prompt_yes_no not given}"
-    local var_to_store_to="${2:?variable to store output for prompt_yes_no not given}"
+    local prompt var_to_store_to prompt_output
+    prompt="${1:?variable for prompt for prompt_yes_no not given}"
+    var_to_store_to="${2:?variable to store output for prompt_yes_no not given}"
     echo -ne "${prompt} [${BGreen-}y${NC}/${BRed-}n${NC}]: "
     IFS= read -r prompt_output
     case "${prompt_output}" in
         [Yy]*)
             declare -g "${var_to_store_to}=Y"
-            unset prompt_output
             return 0
             ;;
         [Nn]*)
             declare -g "${var_to_store_to}=N"
-            unset prompt_output
             return 0
             ;;
         *)
