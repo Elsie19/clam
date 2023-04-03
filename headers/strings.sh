a408ed0e0f0f9fea9d38d02753b2c44cecd6c2ae
Henry Wenger-Stickel <hwengerstickel@protonmail.com>
2023-04-03 18:59:03 -0500

    Add `strings.sh`

--- headers/strings.sh
+++ headers/strings.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+# @file strings.sh
+# @brief A library for interacting with strings.
 
+# @section Functions
+# @description Functions for interacting with strings.
+
+# @description Reverses a string.
+# @stdout Reversed string.
+#
+# @example
+#   strings.rev "foobarbaz"
+#
+# @arg $@ string A string to reverse.
 function strings.rev() {
     local str string i
     string="${*:?No input given to strings.rev}"
@@ -9,16 +21,39 @@ function strings.rev() {
     echo "${str:?Could not reverse string}"
 }
 
+# @description Strips leading whitespace.
+# @see strings.strip
+# @stdout Stripped input.
+#
+# @example
+#   strings.strip_leading "     foobarbaz"
+#
+# @arg $1 string A string to strip.
 function strings.strip_leading() {
     local string="${1:?No input given to strings.strip_leading}"
     echo "${string#"${string%%[![:space:]]*}"}"
 }
 
+# @description Strips trailing whitespace.
+# @see strings.strip
+# @stdout Stripped input.
+#
+# @example
+#   strings.strip_trailing "foobarbaz   "
+#
+# @arg $1 string A string to strip.
 function strings.strip_trailing() {
     local string="${1:?No input given to strings.strip_trailing}"
     echo "${string%"${string##*[![:space:]]}"}"
 }
 
+# @description Strips leading and trailing whitespace.
+# @stdout Stripped input.
+#
+# @example
+#   strings.strip "     foobarbaz   "
+#
+# @arg $1 string A string to strip.
 function strings.strip() {
     local string
     local input="${1:?No input given to strings.strip}"
