04a988fc5e21ffe3c27f712791c9e60fc9bc66e0
Henry Wenger-Stickel <hwengerstickel@protonmail.com>
2023-04-03 18:19:15 -0500

    Add `assert.sh` and `error.sh`

--- headers/assert.sh
+++ headers/assert.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
+# @file assert.sh
+# @brief A library for asserting information.
 
+# @section Functions
+# @description Functions for asserting information.
+
+# @description Checks for root.
+# @noargs
+#
+# @example
+#   if assert.is_root; then
+#       echo "I am root"
+#   fi
+#
+# @exitcode 0 If is root.
+# @exitcode 1 If is not root.
 function assert.is_root() {
     return $(("${EUID}" == 0 ? 0 : 1))
 }
--- headers/error.sh
+++ headers/error.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+# @file error.sh
+# @brief A library for bash-like error messages.
 
+# @section Functions
+# @description Functions for bash-like error messages.
+
+# @description Displays an error message
+#
+# @example
+#   error.error "'line' not defined"
+#   error.error "'line' not defined" 1
+#
+# @arg $1 string Error message
+# @arg $2 optional Exit code
 function error.error() {
     local err_msg="${1:-"Unknown Error"}"
     local err="${2-}"
