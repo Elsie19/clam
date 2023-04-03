e6aa2d31bf01349be52261c3c9a344f25028a577
Henry Wenger-Stickel <hwengerstickel@protonmail.com>
2023-04-03 18:02:02 -0500

    Begin work on documenting entire codebase

--- headers/array.sh
+++ headers/array.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+# @file array.sh
+# @brief A library for interacting with arrays.
 
+# @section Functions
+# @description Functions for interacting with arrays.
+
+# @description Converts a string to an array.
+#
+# @example
+#   array.string_to_array "foobarbaz" to_array
+#
+# @arg $1 string A string to convert.
+# @arg $2 string An array to put the split string into.
 function array.string_to_array() {
     local input array_to
     input="${1:?No input given to array.string_to_array}"
@@ -10,6 +22,17 @@ function array.string_to_array() {
     return $?
 }
 
+# @description Pops an array index out
+#
+# @example
+#   array=(1 2 3 4)
+#   array.pop array 0
+#
+# @arg $1 string An array name
+# @arg $2 string An index to remove
+#
+# @exitcode 0 If successful.
+# @exitcode 1 If array is readonly.
 function array.pop() {
     local to_pop array_name
     # `declare -n` is basically like a pointer to an array
@@ -25,6 +48,17 @@ function array.pop() {
     fi
 }
 
+# @description Remove a key from an array
+#
+# @example
+#   array=(1 2 3 4)
+#   array.remove array 1
+#
+# @arg $1 string An array name
+# @arg $2 string A key to remove
+#
+# @exitcode 0 If successful.
+# @exitcode 1 If array is readonly.
 function array.remove() {
     local array_name to_remove i
     declare -n array_name="${1:?No array given to array.remove}"
@@ -56,25 +90,58 @@ function array.remove() {
     return 0
 }
 
+# @description Check if array contains a key
+#
+# @example
+#   array=(1 2 3 4)
+#   array.contain array 3
+#
+# @arg $1 string An array name
+# @arg $2 string A key to search for
+#
+# @exitcode 0 If array contains key.
+# @exitcode 1 If array does not contain key.
 function array.contain() {
-    local item to_search i
-    item="${1:?No \$1 input given to array.contain}"
-    shift
-    to_search=("${@:?No \$@ given to array.contain}")
-    for i in "${to_search[@]}"; do
-        if [[ ${item} == "${i}" ]]; then
+    local to_search arr i
+    declare -n arr="${1:?No array given to array.contain}"
+    to_search="${2:?No element given to array.contain}"
+    for i in "${arr[@]}"; do
+        if [[ ${to_search} == "${i}" ]]; then
             return 0
         fi
     done
     return 1
 }
 
+# @description Join array by character
+#
+# @example
+#   array=(1 2 3 4)
+#   array.join arr ','
+#
+# @arg $1 string An array name
+# @arg $2 string A character to split IFS
+#
+# @stdout An array with IFS split by supplied character
 function array.join() {
-    local IFS="${1:?No IFS given to array.join}"
-    shift
-    echo "$*"
+    local IFS array_name
+    declare -n array_name="${1:?No array given to to array.join}"
+    local IFS="${2:?No IFS given to array.join}"
+    echo "${array_name[*]}"
 }
 
+# @description Fill array with input
+#
+# @example
+#   array.fill 0 50 "blarb" array
+#
+# @arg $1 integer A starting index for the array
+# @arg $2 integer Length of array fill
+# @arg $3 Any Text to fill array elements with
+# @arg $4 string Array to fill
+#
+# @exitcode 0 If array was successfully filled.
+# @exitcode 1 If array is readonly.
 function array.fill() {
     local i arr start_index count value
     start_index="${1:?No start index given to array.fill}"
