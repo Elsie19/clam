3db72c9c0d69e0240e462e069aac0a6381d19f05
Elsie <elsie19@pm.me>
2024-04-13 18:48:41 -0500

    We gotta expand them aliases lol

--- headers/default.sh
+++ headers/default.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Thanks https://github.com/modernish/modernish/blob/e1af50de312ff334cbfae712c1c5b4eeff810a82/bin/modernish#L291C1-L291C29
+shopt -s expand_aliases
 alias success='{ let "$?==0"; }'
